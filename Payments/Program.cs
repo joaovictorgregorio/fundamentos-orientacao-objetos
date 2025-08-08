55d36d3b2af0969e70a89bff0e90685bccb34c71
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2025-08-07 23:07:15 -0300

    Atualizando repositório, estudos sobre Encapsulamento, Abstração e Herança paradgima da Orientação a Objetos.

---
+++ Payments/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Payments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var pagamentoBoleto = new PagamentoViaBoleto();
+            pagamentoBoleto.Vencimento = DateTime.Now;
+            pagamentoBoleto.Pagar();
+            pagamentoBoleto.NumeroBoleto = "1234567";
+        }
+    }
+
+    class Pagamento
+    {
+        // Propriedades
+        public DateTime Vencimento;
+
+        // Métodos
+        public void Pagar() =>  Console.WriteLine("Hello, world!"); 
+    }
+
+    class PagamentoViaBoleto : Pagamento
+    {
+        public string NumeroBoleto;
+    }
+    
+}
