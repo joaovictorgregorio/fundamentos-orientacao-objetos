7a5ffefb5fa60230f1b931b45cd0a749fcf726e3
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2025-08-10 22:15:38 -0300

    Estudo sobre Modificadores de acesso no C#. Private, protected, internal e public.

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -8,47 +8,26 @@ namespace Payments
         {
             Console.Clear();
 
-            var pagamentoBoleto = new PagamentoViaBoleto();
-            pagamentoBoleto.Vencimento = DateTime.Now;
-            pagamentoBoleto.Pagar();
-            pagamentoBoleto.NumeroBoleto = "1234567";
-
             var pagamento = new Pagamento();
-            Console.WriteLine(pagamento.ToString());
 
-            var pagamentoCartaoCredito = new PagamentoCartaCredito();
-            pagamentoCartaoCredito.NumeroCartao = "12324343434";
-            pagamentoCartaoCredito.ConfirmarNumeroValidoCartaoCredito();
         }
     }
 
-    class Pagamento
+    // 4 modificadores de acesso no C#. Private, protected, internal e public
+    public class Pagamento
     {
         // Propriedades
-        public DateTime Vencimento;
+        protected DateTime Vencimento;
 
         // Métodos
-        public virtual void Pagar()
-            => Console.WriteLine("Hello, world!");
-
-        public override string ToString()
-            => Vencimento.ToString("dd/MM/yyyy");
-        
-    }
-
-    class PagamentoViaBoleto : Pagamento
-    {
-        public string NumeroBoleto;
-        public override void Pagar()
-            => Console.WriteLine("Pagou o boleto!");
-        
-    }
-
-    class PagamentoCartaCredito : Pagamento
-    {
-        public string NumeroCartao;
-        public void ConfirmarNumeroValidoCartaoCredito()
-            => Console.WriteLine($"{NumeroCartao}, foi válidado com sucesso!");
+        private void Pagar() { }
     }
     
+    public class PagamentoBoleto : Pagamento
+    {
+        void Teste()
+        {
+            base.
+        }
+    }
 }
