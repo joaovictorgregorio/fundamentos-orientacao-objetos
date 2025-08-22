717b880c4b01209c8573fb56fdf997c9c30a4ca2
João Silva <joaogregoriodsilva@gmail.com>
2025-08-22 15:08:19 -0300

    Inicio novo módulo 'mão na massa' praticando diversos conceitos da orientação a objetos em uma aplicação real.

---
+++ Balta/ContentContext/Content.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Balta.ContentContext
+{
+    public abstract class Content
+    {
+        public Content()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Balta/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Balta.ContentContext;
+
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var course = new Course();
+
+        }
+    }
+}
